cc74d1a75a510a61fa2c1cb53494725e0f0667a6
AleksaIlicc <aleksa.ilic.dv@outlook.com>
2024-03-16 23:01:14 +0100

    fixed responsiveness for main-image back text

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
-    let scrollPosition = window.scrollY;
-    let viewportHeight = window.innerHeight;
+    const scrollPosition = window.scrollY;
+    const viewportHeight = window.innerHeight;
 
     if (scrollPosition >= viewportHeight) {
         document.querySelector('.navbar').classList.add('gradient');
@@ -8,3 +8,17 @@ window.addEventListener('scroll', function() {
         document.querySelector('.navbar').classList.remove('gradient');
     }
 });
+
+window.addEventListener('resize', function() {
+    const viewportHeight = window.innerHeight;
+
+    const image911 = document.querySelector('.image-overlay');
+    const image911Height = parseInt(window.getComputedStyle(image911).fontSize);
+
+    const topPosition = (viewportHeight) * 0.3 - image911Height;
+    
+    image911.style.top = topPosition + 'px';
+
+});
+
+window.dispatchEvent(new Event('resize'));
